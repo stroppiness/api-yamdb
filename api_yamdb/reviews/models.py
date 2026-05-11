2bea15c60e796fcb90f05aad7e4f0c67e6bab533
Sapar <saparmammet.seyidov@gmail.com>
2026-05-11 19:38:40 +0500

    Sapar's second commit

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -115,7 +115,7 @@ class Review(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE,
                                verbose_name='Автор отзыва')
-    score = models.IntegerField(
+    score = models.PositiveSmallIntegerField(
         'Оценка',
         validators=[MinValueValidator(1), MaxValueValidator(10)]
     )
